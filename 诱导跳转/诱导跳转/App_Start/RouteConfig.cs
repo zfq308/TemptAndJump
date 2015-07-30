ac60cedc7bab4f07ddd7e5e3a0356a5d6d8c91d9
hiliqi <4780044@qq.com>
2015-07-30 17:13:41 +0800

    4

--- 诱导跳转/诱导跳转/App_Start/RouteConfig.cs
+++ 诱导跳转/诱导跳转/App_Start/RouteConfig.cs
@@ -15,20 +15,28 @@ namespace 诱导跳转
 
             routes.MapRoute(
                 name: null,
+                url: "{controller}/{action}/{company}",
+                defaults: new { controller = "Test", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: null,
                 url: "{uid}-{pid}",
-                defaults: new { controller = "Home", action = "Index" }
+                defaults: new { controller = "Test", action = "Index" }
                 );
 
             routes.MapRoute(
                 name: null,
                 url: "{controller}/{action}/{uid}-{pid}",
-                defaults: new { controller = "Home", action = "Index" }
+                defaults: new { controller = "Test", action = "Index" }
                 );
+
+            
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}",
-                defaults: new { controller = "Home", action = "Index" }
+                defaults: new { controller = "Test", action = "Index" }
             );
         }
     }
---
+++ 诱导跳转/诱导跳转/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace 诱导跳转.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index(string company)
+        {
+            ViewBag.Url = GetInfoFromXml(company);
+            return View();
+        }
+
+        private string GetInfoFromXml(string company) //从xml配置文件中取出下载链接
+        {
+            string result = string.Empty;
+            XDocument doc = XDocument.Load(HttpContext.Server.MapPath("~/App_Data/testurl.xml"));
+            var q = from d in doc.Descendants("test") select d;
+            result = q.Where(a => a.Element("company").Value == company).FirstOrDefault().Element("url").Value;
+            return result;
+        }
+    }
+}
